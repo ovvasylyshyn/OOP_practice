bb1b4ce0432368c13ec1ff2898ebfe8dc42f11f7
Vlad <fromvladvasylyshyn@gmail.com>
2023-10-27 12:10:19 +0300

    add class

---
+++ Apartment.h
@@ -0,0 +1,11 @@
+
+#pragma once
+#include <iostream>
+using namespace std;
+class Apartment {
+	int id, number, floor, numOfRooms;
+	float area;
+	string street;
+	bool sunSide, cornerApar;
+public:
+};
